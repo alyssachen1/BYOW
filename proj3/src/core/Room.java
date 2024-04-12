b036cb5ab5130dacf44da3895043605e45f05ff7
Alyssa Chen <alyssa_chen1@berkeley.edu>
2024-04-11 19:02:32 -0700

    changes

--- proj3/src/core/Room.java
+++ proj3/src/core/Room.java
@@ -1,4 +1,5 @@
 package core;
+import edu.princeton.cs.algs4.StdDraw;
 import utils.RandomUtils;
 import utils.FileUtils;
 import java.util.*;
@@ -16,21 +17,24 @@ public class Room {
 
     private static final double maxSpacing = 8.0;
 
+    Random random = new Random();
+
+    World world;
 
 
-    public Room() {
-
+    public Room(World world) {
+        // make the shape
+        this.world = world;
     }
 
     public void generateRoom() {
         // minimum dimensions : 4x4?
         // max dimension : 10x10?
         // also add minimum spacing parameter between rooms (?)
-        Random random = new Random();
-        double width = RandomUtils.uniform(random, minWidth, maxHeight);
+        double width = RandomUtils.uniform(random, minWidth, maxWidth);
         double height = RandomUtils.uniform(random, minHeight, maxHeight);
         double spacing = RandomUtils.uniform(random, minSpacing, maxSpacing);
-
+        roomShape(random.nextInt(), random.nextInt(), width, height);
     }
 
     private void fillFloor() {
@@ -41,6 +45,26 @@ public class Room {
 
     }
 
-    private
+    private void roomShape(double x, double y, double width, double height) {
+        int methodNumber = random.nextInt(3) + 1; // Generates 1, 2, or 3
+
+        switch (methodNumber) {
+            case 1:
+                StdDraw.filledSquare(x, y, width / 2);
+                break;
+            case 2:
+                StdDraw.filledRectangle(x, y, width / 2, height / 2);
+                break;
+            case 3:
+                StdDraw.filledCircle(x, y, height / 2);
+                break;
+            default:
+                System.out.println("Invalid method number");
+        }
+    }
+
+    private boolean overlap() { //implement
+        return false;
+    }
 
 }
