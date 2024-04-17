1be69547dc39f5b27747de28ab0bb8e7236f0fe0
kinton <kintond15@berkeley.edu>
2024-04-16 18:44:32 -0700

    ewf

--- proj3/src/core/Main.java
+++ proj3/src/core/Main.java
@@ -4,7 +4,7 @@ import tileengine.TERenderer;
 
 public class Main {
     public static void main(String[] args) {
-        World world = new World("N20938s");
+        World world = new World("N12345s");
         world.runGame();
     }
 }
